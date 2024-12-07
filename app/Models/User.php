34c1201a0f7fcf850646a09f3b8ad6da5238016f
newtonjob <jobnewton3@gmail.com>
2024-12-07 14:24:46 +0100

    Add helper `User::isAdmin()`

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,12 @@ class User extends Authenticatable
     {
         return $this->hasMany(Book::class);
     }
+
+    /**
+     * Determine if the user is an admin.
+     */
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
 }

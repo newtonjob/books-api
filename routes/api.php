81a2e32c2fce0b87bc4acf3955d999b503682764
newtonjob <jobnewton3@gmail.com>
2024-12-07 14:07:28 +0100

    Add the login route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthTokenController;
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->apiResource('books', BookController::class);
+Route::post('/login', [AuthTokenController::class, 'store']);
 
+Route::middleware('auth:sanctum')->apiResource('books', BookController::class);

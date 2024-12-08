4fed0a83ce8f08bef82d721eefa0ebe94852d2bd
newtonjob <jobnewton3@gmail.com>
2024-12-08 14:18:02 +0100

    Create test author and admin in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Test Admin',
+            'email' => 'admin@example.com',
+            'role' => 'admin',
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Test Author',
+            'email' => 'author@example.com',
+            'role' => 'author',
         ]);
     }
 }
